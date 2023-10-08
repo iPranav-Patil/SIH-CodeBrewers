3e0c9617c3c4ed7cf9f2fe690b86db3a9c194d2f
Pranav Patil <spydycool@gmail.com>
2023-10-08 16:53:51 +0530

    recent

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Companymain from './components/Company Login-Register/Companymain'
 import Railwaymain from './components/Railway Login-Register/Railwaymain'
 import Landing from './components/Landing Page/Landing'
-import MapComponent from './components/Map API/Map'
+// import MapComponent from './components/Map API/Mapper'
 // import Rakeanimation from './components/Visualizer/Rakeanimation'
 import CompanyDashboard from './components/Dashboard/companyDashboard'
 
@@ -14,7 +14,7 @@ function App() {
       <Route path='/' element={<Landing />}></Route>
       <Route path='/companymain' element={<Companymain />}></Route>
       <Route path='/railwaymain' element={<Railwaymain />}></Route>
-      <Route path='/map' element={<MapComponent />}></Route>
+      {/* <Route path='/map' element={<MapComponent />}></Route> */}
       {/* <Route path='/rake' element={<Rakeanimation />}></Route> */}
       <Route path='/cdash' element={<CompanyDashboard />}></Route> 
     </Routes>
